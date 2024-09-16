e1408810590fa86ea4d3ef76e730a30a4268a4a4
gitmbints <rahmbints@gmail.com>
2024-09-16 23:14:18 +0300

    refactor: use async pipe on response and remove subscribe on movies component

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,27 +1,23 @@
+import { Observable } from 'rxjs';
 import { Component, inject, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
 import { DiscoverPageContent } from '../../models/response';
 import { MoviesService } from '../../services/movies.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.css',
 })
 export class MoviesComponent implements OnInit {
-  response!: DiscoverPageContent<Movie>;
-  movies!: Movie[];
+  response$!: Observable<DiscoverPageContent<Movie>>;
 
   moviesService = inject(MoviesService);
 
   ngOnInit(): void {
-    this.moviesService.loadMovies().subscribe({
-      next: (response) => {
-        this.response = response;
-        this.movies = this.response.results;
-      },
-    });
+    this.response$ = this.moviesService.loadMovies();
   }
 }
