860a538a66d4c8e86d7511f269d59290a01a76e3
gitmbints <rahmbints@gmail.com>
2024-09-24 23:38:06 +0300

    feat: add toast animation

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,6 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ToastService } from '../../../services/toast.service';
 import { AsyncPipe } from '@angular/common';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-toast',
@@ -8,12 +15,15 @@ import { AsyncPipe } from '@angular/common';
   imports: [AsyncPipe],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
+  animations: [
+    trigger('toastTrigger', [
+      state('open', style({ transform: 'translateY(0%)' })),
+      state('close', style({ transform: 'translateY(100%)' })),
+      transition('open <=> close', [animate('300ms 100ms ease-in-out')]),
+    ]),
+  ],
 })
 export class ToastComponent {
-  toastState!: string[];
-  toastMessage!: string;
-  showsToast!: boolean;
-
   toast: ToastService = inject(ToastService);
 
   constructor() {}
