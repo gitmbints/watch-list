3fe74f8b37c2ddb3398f5c9910246156377a350d
Ambinintsoa <amr@alternateeve.com>
2024-09-25 15:54:13 +0300

    feat: animate toast notification

--- src/app/components/shared/toast/toast.component.ts
+++ src/app/components/shared/toast/toast.component.ts
@@ -1,6 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ToastService } from '../../../services/toast.service';
 import { AsyncPipe } from '@angular/common';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-toast',
@@ -8,12 +15,27 @@ import { AsyncPipe } from '@angular/common';
   imports: [AsyncPipe],
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
+  animations: [
+    trigger('showToast', [
+      state(
+        'open',
+        style({
+          opacity: 1,
+          transform: 'translateY(0)',
+        }),
+      ),
+      state(
+        'close',
+        style({
+          opacity: 0,
+          transform: 'translateY(100%)',
+        }),
+      ),
+      transition('open <=> close', [animate('200ms ease-in-out')]),
+    ]),
+  ],
 })
 export class ToastComponent {
-  toastState!: string[];
-  toastMessage!: string;
-  showsToast!: boolean;
-
   toast: ToastService = inject(ToastService);
 
   constructor() {}
