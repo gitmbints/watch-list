ffe86d2c9903efc55355aabe5cbf5315aec05e7f
Ambinintsoa <amr@alternateeve.com>
2024-09-25 17:39:08 +0300

    fix: fix the bug on deleting tv-series from watchlist

--- src/app/components/must-watch/must-watch-list/must-watch-list.component.ts
+++ src/app/components/must-watch/must-watch-list/must-watch-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Movie, TvSerie } from '../../../models/movie';
 import { MustWatchItemComponent } from './must-watch-item/must-watch-item.component';
 import { MoviesService } from '../../../services/movies.service';
 import { TOAST_STATE, ToastService } from '../../../services/toast.service';
 import { Response } from '../../../models/response';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-must-watch-list',
@@ -12,34 +13,59 @@ import { Response } from '../../../models/response';
   templateUrl: './must-watch-list.component.html',
   styleUrl: './must-watch-list.component.css',
 })
-export class MustWatchListComponent {
+export class MustWatchListComponent implements OnInit {
   moviesService: MoviesService = inject(MoviesService);
   toast: ToastService = inject(ToastService);
 
   movies = input<Movie[]>();
   tvSeries = input<TvSerie[]>();
 
-  handleDelete(id: number) {
-    const itemToDelete =
-      this.movies()?.find((item) => item.id === id) ||
-      this.tvSeries()?.find((item) => item.id === id);
+  moviesState: Movie[] | undefined;
+  tvSeriesState: TvSerie[] | undefined;
 
-    if (itemToDelete) {
-      const deleteMethod = this.movies()?.some((item) => item === itemToDelete)
-        ? this.moviesService.deleteMovieFromWatchList(id)
-        : this.moviesService.deleteTvSerieFromWatchList(id);
+  ngOnInit(): void {
+    this.moviesState = this.movies();
+    this.tvSeriesState = this.tvSeries();
+  }
 
-      deleteMethod.subscribe({
-        next: (response: Response) => {
-          if (response.success) {
-            this.toast.showToast(TOAST_STATE.success, response.status_message);
-            this.toast.dismissToastAfterDelay();
+  handleDelete(id: number): void {
+    const isMovie = this.moviesState?.some((item) => item.id === id);
+    const deleteMethod = isMovie
+      ? this.deleteMovie(id)
+      : this.deleteTvSeries(id);
+
+    deleteMethod.subscribe({
+      next: (response: Response) => {
+        if (response.success) {
+          if (isMovie) {
+            this.moviesState = this.moviesState?.filter(
+              (item) => item.id !== id,
+            );
+          } else {
+            this.tvSeriesState = this.tvSeriesState?.filter(
+              (item) => item.id !== id,
+            );
           }
-        },
-        error: (err) => {
-          this.toast.showToast(TOAST_STATE.danger, err);
-        },
-      });
-    }
+          this.toast.showToast(TOAST_STATE.success, response.status_message);
+        } else {
+          this.toast.showToast(TOAST_STATE.danger, response.status_message);
+        }
+        this.toast.dismissToastAfterDelay();
+      },
+      error: (err) => {
+        this.toast.showToast(
+          TOAST_STATE.danger,
+          err.message || 'An error occurred',
+        );
+      },
+    });
+  }
+
+  private deleteMovie(id: number): Observable<Response> {
+    return this.moviesService.deleteMovieFromWatchList(id);
+  }
+
+  private deleteTvSeries(id: number): Observable<Response> {
+    return this.moviesService.deleteTvSerieFromWatchList(id);
   }
 }
